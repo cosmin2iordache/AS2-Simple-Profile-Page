783044db17df53f81d42509901d92e4d3d9254cd
cosmin2iordache <cosmin2iordache@gmail.com>
2020-04-26 06:38:09 +0100

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 /* SCRIPTS.JS */
 
 function changePic() {
-      document.getElementById("profilePic").src = "/images/cos.webp";
+      document.getElementById("profilePic").src = "images/cos.webp";
     }
     
     function normalPic() {
-      document.getElementById("profilePic") .src = "/images/cos1.png";
+      document.getElementById("profilePic") .src = "images/cos1.png";
     }
