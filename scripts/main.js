5829c7cbea2dec018579365611894338bb5154c7
cosmin2iordache <cosmin2iordache@gmail.com>
2020-04-26 06:26:50 +0100

    ready

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+      document.getElementById("profilePic").src = "/images/cos.webp";
+    }
+    
+    function normalPic() {
+      document.getElementById("profilePic") .src = "/images/cos1.png";
+    }
